094d1fd54656433153368162cebde8a7048275a5
numb1086 <numb1086@users.noreply.github.com>
2024-04-30 16:14:40 +0800

    [Update] No need to be used in this stage

--- controllers/userController.js
+++
@@ -1,8 +0,0 @@
-const { User, UserAttraction } = require("../models/userModel");
-
-const init = async (req, res) => {
-  await User.sync();
-  res.json({ message: "ok" });
-};
-
-module.exports = { init };
